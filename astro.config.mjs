26b4a12205c85e7e2e31ed7e7311a2a4136e908f
amat012 <aaron.m91021@gmail.com>
2025-04-03 14:03:24 -0400

    New commit, mostly finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import alpinejs from '@astrojs/alpinejs';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [alpinejs()]
+});
+
+{
+  experimental: {
+    svg: true
+  }
+ }
