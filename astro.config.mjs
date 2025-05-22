2f498d0f13f400598aca142c016dca814ccaa2dc
amat012 <aaron.m91021@gmail.com>
2025-05-22 13:32:57 -0400

    Update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import svelte from "@astrojs/svelte";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
   site: "https://amat012.github.io",
   base: "/ghost-runner-pizza",
   trailingSlash: "always",
+  adapter: netlify(),
 });
 
 {
